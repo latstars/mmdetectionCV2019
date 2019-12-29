f75b92e0717fe2a54a032bbd885c70d62309dc2b
latstars <latstars@qq.com>
2019-12-29 04:46:36 -0500

    anchor target bi

--- mmdet/core/anchor/__init__.py
+++ mmdet/core/anchor/__init__.py
@@ -3,8 +3,13 @@ from .anchor_target import anchor_inside_flags, anchor_target
 from .guided_anchor_target import ga_loc_target, ga_shape_target
 from .point_generator import PointGenerator
 from .point_target import point_target
+# @latstars
+from .anchor_target_bi import anchor_target_bi
+from .anchor_target_bi_visible import anchor_target_bi_visible
 
 __all__ = [
     'AnchorGenerator', 'anchor_target', 'anchor_inside_flags', 'ga_loc_target',
     'ga_shape_target', 'PointGenerator', 'point_target'
+    # @latstars
+    ,'anchor_target_bi', 'anchor_target_bi_visible'
 ]
